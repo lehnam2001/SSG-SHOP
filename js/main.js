e06aae278d6497c97af16763f503679c30df125a
LH <lehnam2991@gmail.com>
2021-09-22 23:28:23 +0700

    upload source code

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// var productItem = document.getElementsByClassName('cart-product-item');
+// console.log(productItem);
+
+// var nodeGiaSanPham = productItem[2];
+// console.log(nodeGiaSanPham);
+
+//  var giaSanPham = nodeGiaSanPham.innerText;
+//  console.log(giaSanPham)
+
+
+//  var nodeTotalSanPham = productItem[4];
+//  console.log(nodeTotalSanPham);
+
+// nodeTotalSanPham.innerText = giaSanPham;
+
+// Hơi buồn vì không làm được total, đã research nhưng không tìm được kết quả cần tìm :((
